c8d2901accb06936f7f56b8b6ab7a8071f0467d2
AlexGeniusMan <alexgeniusman@gmail.com>
2021-09-03 14:10:06 +0300

    meh

--- chart.py
+++ chart.py
@@ -1,8 +1,39 @@
+import csv
+
 import pandas as pd
 import matplotlib.pyplot as plt
+import math
 
-data = pd.read_csv('./RDS-B.csv')
-df = pd.DataFrame(data, columns=['Date', 'Close'])
-print(df)
-df.plot(x="Date", y=["Close"])
-plt.show()
+v = open('./RDS-B.csv')
+r = csv.reader(v)
+row0 = r.next()
+row0.append('berry')
+print(row0)
+for item in r:
+    item.append(item[0])
+    print(item)
+
+# data = pd.read_csv('./RDS-B.csv')
+# df = pd.DataFrame(data, columns=['Date', 'Close'])
+# # df = pd.DataFrame(
+# #     {'date': df_st.Date, 'steps': df_st['Close']})
+# print(df)
+# df.plot(x="Date", y=["Close"])
+# plt.show()
+
+# x = pd.DataFrame(data, columns=['Date'])
+# y = pd.DataFrame(data, columns=['Date'])
+#
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+#
+# plt.plot(x, y)
+# plt.xlim(-3, 3)
+# plt.ylim(-3, 3)
+#
+# ax.set_aspect('equal', adjustable='box')
+#
+# plt.xlabel("x")
+# plt.ylabel("sinx")
+#
+# plt.show()
